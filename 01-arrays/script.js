6f1ad649e915ee2449dbc74a7d1fc554de992559
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-29 15:42:25 +0200

    Refactor teacher array operations for consistency and clarity

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -16,22 +16,33 @@ const teachers = [
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
-  teachers.splice(4, 1 , "Patrick");
+  teachers.splice( 4, 1 , 'Patrick' );
   console.log(teachers)
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+
+  const lastTeacher = teachers.pop();
+  console.log(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+  
+  const firstTeacher = teachers.shift();
+  console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
+  const newLastTeacher = teachers.push( 'Vanessa' );
+  console.log(teachers)
+
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+
+  const newFirstTeacher = teachers.unshift( 'Sara')
+  console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+
+  const lewisIndex = null;
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
