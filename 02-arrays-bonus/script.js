bfb1eff4f43756cf9f7aafeecb270683a514f797
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-29 16:33:19 +0200

    Fix formatting and remove 'Ed' from teachers array

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -15,9 +15,13 @@ console.log(reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+
+const longNames = null
 
 // 3. Rimuovi 'Ed' dall'array teachers
+
+teachers.splice(5, 1)
+console.log(teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
