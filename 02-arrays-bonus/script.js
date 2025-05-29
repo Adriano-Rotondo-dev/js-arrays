f7d6ca0bb53c9524bc806cf55a4e09578fffc9a2
Adriano Rotondo <adriano.rotondo.dev@gmail.com>
2025-05-29 17:13:23 +0200

    Fix logic for creating 'longNames' array and update 'isFabioPresent' check

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -17,8 +17,13 @@ console.log(reversedTeachers)
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 
-const longNames = null
-
+const longNames = [ ]
+for (let i = 0; i < teachers.length; i ++ ) {
+  if (teachers.lenght >= 5) {
+    longNames.push(teachers)
+  } 
+}
+  console.log(longNames)
 // 3. Rimuovi 'Ed' dall'array teachers
 
 teachers.splice(5, 1)
@@ -27,10 +32,9 @@ console.log(teachers)
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 
-const isFabioPresent = teachers.indexOf('Fabio');
-if (isFabioPresent !== -1 ) {
-  console.log("Fabio is present")
+let isFabioPresent = false 
+for ( let i = 0; i < teachers.length; i++ ) {
 }
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+let teachersString = null;
